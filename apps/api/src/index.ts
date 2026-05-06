ff7e01b707b7b863bbff8e9de651949f25644186
hetpatel4381 <hetpatel2808hp@gmail.com>
2026-05-06 20:36:24 +0530

    feat(api): prisma setup with postgres connection

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,21 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
 
+// Initialize Prisma Client with adapter
+const prisma = new PrismaClient();
+
 app.get("/", async () => {
   return { message: "API running 🚀" };
+});
+
+app.get("/test-db", async () => {
+  const data = await prisma.test.create({
+    data: { name: "working ✅" },
+  });
+
+  return data;
 });
 
 app.listen({ port: 3000 }, (err, address) => {
